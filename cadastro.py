6bf4bccd73a805ae45ed9252eae719031f76cbaa
Ana Vitória <anavp.apolinario@gmail.com>
2026-09-09 13:44:13 -0300

    Commit 4: Novas funções para consulta

--- cadastro.py
+++ cadastro.py
@@ -1,6 +1,6 @@
 # Sistema de Cadastro de Pessoas - versao 2
 # novos requisitos: menu, consulta, alteracao e listagem
- 
+
 def exibir_menu():
     print("=========================")
     print(" CADASTRO DE PESSOAS")
@@ -13,7 +13,6 @@ def exibir_menu():
     return int(input("Escolha uma opcao: "))
 
 def cadastrar_pessoa(nomes, idades, emails):
-   
     nome = input("Informe o nome: ")
     nomes.append(nome)
     idade = int(input("Informe a idade: "))
@@ -25,31 +24,47 @@ def cadastrar_pessoa(nomes, idades, emails):
     else:
         print("Situação: Menor de idade.")
 
+# --- NOVAS FUNÇÕES DO COMMIT 4 ---
+
+def buscar_pessoa(nome_busca, nomes):
+    pos = 0
+    while pos < len(nomes):
+        if nomes[pos] == nome_busca:
+            return pos
+        pos += 1
+    return -1
+
+def exibir_pessoa(pos, nomes, idades, emails):
+    print("Nome: " + nomes[pos])
+    print("Idade: " + str(idades[pos]))
+    print("E-mail: " + emails[pos])
+
+def consultar_pessoa(nomes, idades, emails):
+    nome_busca = input("Nome para consultar: ")
+    pos = buscar_pessoa(nome_busca, nomes)
+    
+    if pos != -1:
+        exibir_pessoa(pos, nomes, idades, emails)
+    else:
+        print("Usuário não encontrado")
+
+# ---------------------------------
+
 nomes = []
 idades = []
 emails = []
- 
+
 op = 0
 
 while op != 5:
     op = exibir_menu()
- 
+
     if op == 1:
         cadastrar_pessoa(nomes, idades, emails)
     
     elif op == 2:
-        nome_busca = input("Nome para consultar: ")
-        pos = 0
-        achou = 0
-        while pos < len(nomes):
-            if nome_busca == nomes[pos]:
-                print("Nome: " + nomes[pos])
-                print("Idade: " + str(idades[pos]))
-                print("E-mail: " + emails[pos])
-                achou = 1
-            pos = pos + 1
-        if achou == 0:
-            print("Usuário não encontrado")
+        consultar_pessoa(nomes, idades, emails)
+
     elif op == 3:
         nome_alterar = input("Nome para alterar: ")
         pos = 0
@@ -71,10 +86,11 @@ while op != 5:
             print("Idade: " + str(idades[pos]))
             print("E-mail: " + emails[pos])
             pos = pos + 1
+            
     elif op == 5:
         print("Saindo...")
- 
+
     else:
         print("Opcao invalida")
- 
+
 print("Fim do programa")
